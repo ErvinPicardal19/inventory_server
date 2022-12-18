fc96f4500cdf7fc396928c8939f3d6ccf00497f6
Ervin Picardal <ervinjohnpicardal@gmail.com>
2022-12-19 00:06:34 +0800

    Deploy

--- config/corsWhiteList.js
+++ config/corsWhiteList.js
@@ -1,9 +1,5 @@
 const whiteList = [
-   "https://www.yoursite.com",
-   "http://localhost:5500",
-   "http://127.0.0.1:5500",
-   "http://localhost:3000",
-   "http://192.168.1.57:3000"
+   "https://malasakit.onrender.com",
 ]
 
 module.exports = whiteList;
